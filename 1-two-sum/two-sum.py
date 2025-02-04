56db424b5a8e88b2c49adce7926e24196210fc06
Vitor Messias <vitorhsm14@gmail.com>
2025-02-04 17:21:13 -0300

    [0ms - 100%] | [19.41MB - 5.90%]

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        m = {}
+        for i in range(len(nums)):
+            if nums[i] in m:
+                return [m[nums[i]], i]
+            m[target - nums[i]] = i
