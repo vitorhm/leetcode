4f2a9908874bfa996c5f4c8bafa28fdba2197553
Vitor Messias <vitorhsm14@gmail.com>
2025-02-11 18:01:56 -0300

    [15ms - 24.60%] | [18.05MB - 16.09%]

---
+++ 1910-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        stack = []
+        len_part = len(part)
+        for c in s:
+            stack.append(c)
+
+            if len(stack) >= len(part) and ''.join(stack[-len_part:]) == part:
+                del stack[-len_part:]
+
+        return ''.join(stack)
